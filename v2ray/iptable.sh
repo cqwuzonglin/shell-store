a309a467495145637797245183976635ba713096
wuzonglin <cqwuzonglin@163.com>
2021-02-22 16:00:17 +0800

    11

--- v2ray/iptable.sh
+++ v2ray/iptable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" ]; then
-	for arg in $*                     
+	for arg in $*
 	do
-	  echo "firewall-cmd --zone=public --add-port=$arg/tcp --permanent"        
-	##  firewall-cmd --zone=public --add-port=$arg/tcp --permanent
+	  echo "firewall-cmd --zone=public --add-port=$arg/tcp --permanent"
+	  firewall-cmd --zone=public --add-port=$arg/tcp --permanent
 	done
 else
 	echo "one more port number request!"
