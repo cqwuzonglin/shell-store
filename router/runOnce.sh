5dce5934061fd6eb9064e73b50154b8a711a66c7
wuzonglin <cqwuzonglin@163.com>
2021-06-11 03:13:39 +0800

    11

--- router/runOnce.sh
+++ router/runOnce.sh
@@ -5,14 +5,17 @@ echo "uptime:$uptime runtime:$runtime"
 if [ "$runtime" -gt "$uptime" ]; then
   echo 'not run'
 else
-  date +%s > /userdisk/runtime
-  echo 'must run'
   IP_ADDR=$(ip -4 addr show dev pppoe-wan | grep 'scope global' | grep -v deprecated | awk -F '/|inet ' 'NR==1{print $2;}'| awk -F '/|peer ' 'NR==1{print $1;}' 2>&1)
-  iptables -t nat -A zone_lan_postrouting -s 192.168.31.0/24 -d 192.168.31.1/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j SNAT --to-source 192.168.31.1
-  iptables -t nat  -A zone_lan_prerouting -s 192.168.31.0/24 -d $IP_ADDR/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j DNAT --to-destination 192.168.31.1:60000-64000
-  iptables -t nat  -A zone_wan_prerouting -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect" -j DNAT --to-destination 192.168.31.1:60000-64000
-  iptables-save
-  nohup /userdisk/nps/npc &
-  /userdisk/webdav/caddy start
+  if [ -n "$IP_ADDR" ]; then
+    date +%s > /userdisk/runtime
+    echo 'must run'
+    iptables -t nat -A zone_lan_postrouting -s 192.168.31.0/24 -d 192.168.31.1/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j SNAT --to-source 192.168.31.1
+    iptables -t nat -A zone_lan_prerouting -s 192.168.31.0/24 -d "$IP_ADDR"/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j DNAT --to-destination 192.168.31.1:60000-64000
+    iptables -t nat -A zone_wan_prerouting -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect" -j DNAT --to-destination 192.168.31.1:60000-64000
+    iptables-save
+    nohup /userdisk/nps/npc &
+    cd /userdisk/webdav/;
+    ./caddy start
+  fi
 
 fi
---
+++ test.sh
@@ -0,0 +1,10 @@
+
+    IP_ADDR=$(ip -4 addr show dev pppoe-wan | grep 'scope global' | grep -v deprecated | awk -F '/|inet ' 'NR==1{print $2;}'| awk -F '/|peer ' 'NR==1{print $1;}' 2>&1)
+    iptables -t nat -A zone_lan_postrouting -s 192.168.31.0/24 -d 192.168.31.1/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j SNAT --to-source 192.168.31.1
+    iptables -t nat -A zone_lan_prerouting -s 192.168.31.0/24 -d "$IP_ADDR"/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j DNAT --to-destination 192.168.31.1:60000-64000
+    iptables -t nat -A zone_wan_prerouting -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect" -j DNAT --to-destination 192.168.31.1:60000-64000
+
+
+    iptables -t nat -D zone_lan_postrouting -s 192.168.31.0/24 -d 192.168.31.1/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j SNAT --to-source 192.168.31.1
+    iptables -t nat -D zone_lan_prerouting -s 192.168.31.0/24 -d "$IP_ADDR"/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j DNAT --to-destination 192.168.31.1:60000-64000
+    iptables -t nat -D zone_wan_prerouting -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect" -j DNAT --to-destination 192.168.31.1:60000-64000
