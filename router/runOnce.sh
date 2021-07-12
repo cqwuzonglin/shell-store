3285fbf1b1ad74a83ad5df15ed0e6d3c46ed0ae3
wuzonglin <cqwuzonglin@163.com>
2021-07-12 15:35:30 +0800

    11

--- router/runOnce.sh
+++ router/runOnce.sh
@@ -9,7 +9,7 @@ if [ -n "$IP_ADDR" ] && [ "$ip" != "$IP_ADDR" ] ; then
 
   if [ -n "$ip" ] ; then
     iptables -t nat -D zone_lan_postrouting -s 192.168.31.0/24 -d 192.168.31.1/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j SNAT --to-source 192.168.31.1;
-    iptables -t nat -D zone_lan_prerouting -s 192.168.31.0/24 -d "$IP_ADDR"/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j DNAT --to-destination 192.168.31.1:60000-64000;
+    iptables -t nat -D zone_lan_prerouting -s 192.168.31.0/24 -d "$ip"/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j DNAT --to-destination 192.168.31.1:60000-64000;
     iptables -t nat -D zone_wan_prerouting -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect" -j DNAT --to-destination 192.168.31.1:60000-64000;
   fi
   iptables -t nat -A zone_lan_postrouting -s 192.168.31.0/24 -d 192.168.31.1/32 -p tcp -m tcp --dport 60000:64000 -m comment --comment "redirect (reflection)" -j SNAT --to-source 192.168.31.1;
