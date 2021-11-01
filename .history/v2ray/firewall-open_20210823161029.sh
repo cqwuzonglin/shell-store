805c8f435a7bf25cdea6ba45c10708e24afda561
wuzonglin <cqwuzonglin@163.com>
2021-11-01 15:26:29 +0800

    11

---
+++ .history/v2ray/firewall-open_20210823161029.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$1" ]; then
+	for arg in $*
+	do
+	  echo "firewall-cmd --zone=public --add-port=$arg/tcp --permanent"
+#	  echo "firewall-cmd --zone=public --add-port=$arg/udp --permanent"
+	  firewall-cmd --zone=public --add-port=$arg/tcp --permanent
+#	  firewall-cmd --zone=public --add-port=$arg/udp --permanent
+	done
+	firewall-cmd --reload
+else
+	echo "one more port number request!"
+fi
+
