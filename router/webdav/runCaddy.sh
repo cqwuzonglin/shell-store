fed94adfba2f0257b7d0a66a7d225a2d3f434367
cqwuzonglin <45115566+cqwuzonglin@users.noreply.github.com>
2022-04-11 23:34:03 +0800

    Update runCaddy.sh

--- router/webdav/runCaddy.sh
+++ router/webdav/runCaddy.sh
@@ -4,3 +4,9 @@ if [ "$isRun" = "0" ] ; then
   cd /userdisk/webdav/;
   ./caddy start;
 fi
+pid=$(ps | grep caddy | grep run | awk -F ' ' '{print $1}')
+if [ $pid -lt 10000 ] ; then
+  cd /userdisk/webdav/;
+  ./caddy stop;
+  ./caddy start;
+fi
