def9f9439d354a210fcecf2ae08f518759ea13a0
wuzonglin <cqwuzonglin@163.com>
2022-04-25 16:33:39 +0800

    11

--- adb/disabled_miui.bat
+++ adb/disabled_miui.bat
@@ -1,97 +1,47 @@
-# 万象熄屏
-adb shell pm disable-user com.miui.aod
-# 停用广告模块
-adb shell pm disable-user com.miui.analytics
-# 停用广告模块
-adb shell pm disable-user com.miui.systemAdSolution
-# 停用广告模块
-adb shell pm disable-user com.xiaomi.ab
-# 停用收音机
-adb shell pm disable-user com.miui.fmservice
-# 停用HTML查看器
-adb shell pm disable-user com.android.htmlviewer
-# 停用快应用
-adb shell pm disable-user com.miui.hybrid.accessory
-# 停用快应用
-adb shell pm disable-user com.miui.hybrid
-# 停用用户反馈
-adb shell pm disable-user com.miui.bugreport
-# 停用服务和反馈
-adb shell pm disable-user com.miui.miservice
-# 停用浏览器
-adb shell pm disable-user com.android.browser
-# 停用搜索
-adb shell pm disable-user com.android.quicksearchbox
-# 停用视频
-adb shell pm disable-user com.miui.video
-# 停用音乐
-adb shell pm disable-user com.miui.player
-# 停用游戏中心
-adb shell pm disable-user com.xiaomi.migameservice
-adb shell pm disable-user com.xiaomi.gamecenter.sdk.service
-# 停用miui+ 万物互联
-adb shell pm disable-user com.xiaomi.mirror
-# 停用 智能助理
-adb shell pm disable-user com.miui.personalassistant 
-#
-# 停用应用商店
-# adb shell pm disable-user com.xiaomi.market
-# 停用系统更新
-# adb shell pm disable-user com.android.updater
-# 停用电子邮件
-# adb shell pm disable-user com.android.email
-# 停用小米钱包、支付
-# adb shell pm disable-user com.xiaomi.miplay_client
-# adb shell pm disable-user com.mipay.wallet
-
-# adb shell pm disable-user com.sohu.inputmethod.sogou.xiaomi
-# 停用搜狗输入法
-
-# 停用【谷歌全家桶】 按任意键继续…
-adb shell pm disable-user com.google.android.ext.services
-adb shell pm disable-user com.google.android.marvin.talkback
-#
-#com.miui.analytics（小米广告分析，必删）
-#com.miui.systemAdSolution（小米系统广告解决方案，必删）
-#com.miui.virtualsim 全球上网
-#com.android.email 电子邮件
-#com.miui.backup 备份
-#com.miui.notes 便签
-#com.miui.hybrid 快应用
-#com.miui.hybrid.accessory 快应用服务
-#com.xiaomi.scanner 扫一扫
-#com.android.quicksearchbox 搜索
-#com.miui.voiceassist 小爱同学
-#com.miui.player 音乐
-#com.miui.video 视频
-#com.miui.backup 备份
-#com.miui.cloudbackup 桌面备份
-#com.miui.bugreport 用户反馈
-#com.miui.touchassistant 悬浮球
-#com.android.printspooler 打印处理服务
-#com.android.bips 默认打印服务
-#com.xiaomi.gamecenter 游戏
-#com.xiaomi.gamecenter.sdk.service 游戏服务
-#com.miui.userguide 用户手册
-#com.miui.cloudservice 小米云服务
-#com.android.midrive 小米云盘
-#com.miui.yellowpage 生活黄页
-#com.miui.screenrecorder 屏幕录制
-#com.android.browser 浏览器
-#com.miui.contentextension 传送门
-#com.android.providers.userdictionary 用户字典
-#com.xiaomi.payment 米币支付
-#com.xiaomi.drivemode 驾车模式
-#com.android.stk USIM卡应用
-#com.xiaomi.simactivate.service 小米SIM卡激活服务
-#com.sohu.inputmethod.sogou.xiaomi 搜狗输入法小米版
-#com.miui.personalassistant 智能助理
-#com.android.calllogbackup 小米云服务中备份通话记录的
-#com.miui.systemAdSolution msa小米广告推送服务
-#com.xiaomi.scanner 扫一扫
-#com.miui.analytics Analytics(未知)
-#com.dsi.ant.server ANT HAL Service(未知)
-#com.svox.pico Pico TTS 语音识别系统
-#com.android.thememanager 个性主题
-#com.miui.compass 指南针
-#com.miui.aod 万象熄屏
+for %%X in (
+  "com.android.bips"
+  "com.android.browser"
+  "com.android.midrive"
+  "com.android.printspooler"
+  "com.android.htmlviewer"
+  "com.bsp.catchlog"
+  "com.miui.aod"
+  "com.miui.miwallpaper"
+  "com.miui.contentextension"
+  "com.miui.miservice"
+  "com.miui.hybrid"
+  "com.miui.hybrid.accessory"
+  "com.miui.yellowpage"
+  "com.miui.fmservice"
+  "com.miui.mishare.connectivity"
+  "com.miui.nextpay"
+  "com.miui.touchassistant"
+  "com.miui.player"
+  "com.miui.bugreport"
+  "com.miui.personalassistant"
+  "com.miui.analytics"
+  "com.miui.translation.kingsoft"
+  "com.miui.translation.xmcloud"
+  "com.miui.translation.youdao"
+  "com.miui.translationservice"
+  "com.miui.vsimcore"
+  "com.miui.phrase"
+  "com.miui.misound"
+  "com.miui.accessibility"
+  "com.miui.vipservice"
+  "com.miui.video"
+  "com.xiaomi.ab"
+  "com.xiaomi.payment"
+  "com.xiaomi.mi_connect_service"
+  "com.xiaomi.gamecenter.sdk.service"
+  "com.xiaomi.migameservice"
+  "com.xiaomi.joyose"
+  "com.xiaomi.mirror"
+  "com.xiaomi.mircs"
+  "com.milink.service"
+  "com.google.android.marvin.talkback"
+  "com.google.android.ext.services"
+) do (
+  @ECHO adb shell pm disable-user %%X
+  adb shell pm disable-user %%X
+)
