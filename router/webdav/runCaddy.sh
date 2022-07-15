c8e02ec80ffd32b29e43e0dfb07202eb69e5170f
wuzonglin <cqwuzonglin@163.com>
2022-07-15 13:48:22 +0800

    webdav

--- router/webdav/runCaddy.sh
+++ router/webdav/runCaddy.sh
@@ -10,3 +10,9 @@ if [ $pid -lt 10000 ] ; then
   ./caddy stop;
   ./caddy start;
 fi
+staus=$(curl --connect-timeout 2 -o /dev/null -s -w %{http_code} http://192.168.31.1:61022/)
+if [ "${staus}" == "000" ] ; then
+  cd /userdisk/webdav/;
+  ./caddy stop;
+  ./caddy start;
+fi
