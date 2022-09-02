14076434b1fa8f043e80cfad88dfe54b283dbcf1
cqwuzonglin <cqwuzonglin@163.com>
2022-09-03 00:12:59 +0800

    111

--- router/webdav/runCaddy.sh
+++ router/webdav/runCaddy.sh
@@ -10,7 +10,7 @@ if [ $pid -lt 10000 ] ; then
   ./caddy stop;
   ./caddy start;
 fi
-staus=$(curl --connect-timeout 2 -o /dev/null -s -w %{http_code} http://192.168.31.1:61022/)
+staus=$(curl --connect-timeout 2 -o /dev/null -s -w %{http_code} http://192.168.5.100:61022/)
 if [ "${staus}" == "000" ] ; then
   cd /userdisk/webdav/;
   ./caddy stop;
--- v2ray/pac/pac_get.sh
+++ v2ray/pac/pac_get.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/env bash
-
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+#!/usr/bash
 
 prefix_suffix(){
 PAC_TAME="/*
