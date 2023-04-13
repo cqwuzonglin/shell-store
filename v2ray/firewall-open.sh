f505f29573dcfcd5d9fb5259a6202a9726898216
wuzonglin <cqwuzonglin@163.com>
2023-04-13 11:09:36 +0800

    11

--- v2ray/firewall-open.sh
+++ v2ray/firewall-open.sh
@@ -3,11 +3,12 @@ if [ "$1" ]; then
 	for arg in $*
 	do
 	  echo "firewall-cmd --zone=public --add-port=$arg/tcp --permanent"
-#	  echo "firewall-cmd --zone=public --add-port=$arg/udp --permanent"
+	  echo "firewall-cmd --zone=public --add-port=$arg/udp --permanent"
 	  firewall-cmd --zone=public --add-port=$arg/tcp --permanent
-#	  firewall-cmd --zone=public --add-port=$arg/udp --permanent
+	  firewall-cmd --zone=public --add-port=$arg/udp --permanent
 	done
 	firewall-cmd --reload
+	firewall-cmd --zone=public --list-ports
 else
 	echo "one more port number request!"
 fi
