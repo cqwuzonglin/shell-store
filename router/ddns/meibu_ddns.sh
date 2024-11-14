3448d5eec28efba43e5ee7726c0adc5cab25b645
wzl <cqwuzonglin@163.com>
2024-11-14 23:11:35 +0800

    ddns

--- router/ddns/meibu_ddns.sh
+++ router/ddns/meibu_ddns.sh
@@ -4,12 +4,12 @@ iptxt="/userdisk/ddns/ip.txt"
 # 获取IPv4地址
 # ip=$(ip -4 addr show dev pppoe-wan | grep 'scope global' | grep -v deprecated | awk -F '/|inet ' 'NR==1{print $2;}' | awk -F '/|peer ' 'NR==1{print $1;}' 2>&1)
 ip=$(curl -s http://www.meibu.com/ips.asp)
-if [ "${ip:-none}" == "none" ] ; then
+if [ "${ip:-none}" == "none" ]; then
   echo "[ddns] 获取IP错误！"
   exit 12
 fi
 
-if [ -f $iptxt ] ; then
+if [ -f $iptxt ]; then
   oldip=$(tail -n 1 $iptxt)
 else
   oldip="::"
@@ -17,8 +17,25 @@ fi
 domain="wzlrouter.x3322.net"
 pingIp=$(ping $domain -c 1 | awk 'NR==2 {print $4}' | sed -n 's/\([0-9\.]*\):/\1/p')
 # 与ip.txt中的IP地址对比，如果一致，则退出；如果不一致，则将新IP写入ip.txt文件中，并提交动态域名更新。
-if [ "$ip" = "$oldip" ] && [ "$ip" = "$pingIp" ] ; then
+if [ "$ip" = "$oldip" ] && [ "$ip" = "$pingIp" ]; then
   echo "[ddns] IP无变化！"
+  minute=$(date +%M)
+
+  # 判断分钟数是否为 15 的倍数
+  if [ $((minute % 15)) -eq 0 ]; then
+    # 将变化的ip地址写入ip.txt
+    echo "$oldip - $ip - $pingIp "
+    echo $ip >$iptxt
+
+    # 更新动态域名IP地址，xxxxxx.noip.cn为申请的域名，“pwd=”后面为密码
+    url="http://members.3322.net/dyndns/update?system=dyndns&hostname=${domain}"
+    re=$(curl -s -u wzlrouter:9D6At7YxyxtK3 $url)
+    echo "[${ip} ${domain}] ${re}！"
+    domain="ducktang"
+    url="https://www.duckdns.org/update?domains=${domain}&token=cdb1c075-6570-482e-bd93-9d0829e0a2de&ip="
+    re=$(curl -s $url)
+    echo "[${ip} ${domain}] ${re}！"
+  fi
 else
   # 将变化的ip地址写入ip.txt
   echo "$oldip - $ip - $pingIp "
